a730f8537e47e4858ae06b6a5afc4d24764d141c
William Wu <william.wu@themathpath.com>
2012-04-08 18:45:13 -0700

    added commuter's lament

--- goodbye.c
+++ goodbye.c
@@ -3,5 +3,13 @@ int main(void) {
 	printf("********************\n");
 	printf("Goodbye Cruel World!\n"); 
 	printf("********************\n");
+	printf("Overslept\n");
+	printf("So tired\n");
+	printf("If late\n");
+	printf("Get fired\n");
+	printf("Why bother?\n");
+	printf("Why the pain?\n");
+	printf("Just go home\n");
+	printf("Do it again.\n");
 	return 0; 
 }
