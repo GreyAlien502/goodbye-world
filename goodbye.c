f6782a05737a4d9d986d061641b486f8051eef8f
GreyAlien502 <greyalien502@gmail.com>
2015-06-27 22:31:47 -0400

    grammar fix

--- goodbye.c
+++ goodbye.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void) {
-	printf("*******************************************************\n");
-	printf("AZH OT YHOYH SHU ZHOZH OT YHOYH OH O OT ZOEN YHIG ZHOZH\n"); 
-	printf("*******************************************************\n");
+	printf("**********************************************************\n");
+	printf("AZH OT YHOYH SHU ZHOZH OT YHOYH OH OH O OT ZOEN YHIG ZHOZH\n"); 
+	printf("**********************************************************\n");
 	return 0; 
 }
